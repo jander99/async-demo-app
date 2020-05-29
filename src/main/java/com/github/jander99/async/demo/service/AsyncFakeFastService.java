df0028193007084a12b8ed1aeacb731ed6fdba91
Jeff Anderson <jeffrey_d_anderson@homedepot.com>
2020-05-28 20:53:25 -0400

    Finish Go server

--- src/main/java/com/github/jander99/async/demo/service/AsyncFakeFastService.java
+++ src/main/java/com/github/jander99/async/demo/service/AsyncFakeFastService.java
@@ -1,14 +1,22 @@
 package com.github.jander99.async.demo.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.CompletableFuture;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class AsyncFakeFastService {
+
+    private final RestTemplate restTemplate;
+
+    private final int port = 8000;
 
     @Async
     public CompletableFuture<Long> asyncWait(long millis) {
@@ -28,6 +36,14 @@ public class AsyncFakeFastService {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
             log.error("Thread {} got interrupted.", Thread.currentThread().getName(), e);
+        }
+    }
+
+    private void externalLocalCall(long millis) {
+        try {
+            restTemplate.getForObject(String.format("http://localhost:%s/?t=%s",port,millis),String.class);
+        } catch (RestClientException rce) {
+            log.error("Uh oh.", rce);
         }
     }
 }
